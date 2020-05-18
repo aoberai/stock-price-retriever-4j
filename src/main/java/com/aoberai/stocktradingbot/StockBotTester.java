e837300b8f9265974f4b6433dc673229bf0121fa
Aditya-Paly <aoberai05@gmail.com>
2020-05-18 10:59:57 -0700

    due to alphavantage not providing live stock price for nasdaq tech companies, this project will come to an end in java. Its simply much easier to get the same data in python and there is more potential as well having access to powerful yahoo finance web scrapers.

--- src/main/java/com/aoberai/stocktradingbot/StockBotTester.java
+++ src/main/java/com/aoberai/stocktradingbot/StockBotTester.java
@@ -1,24 +1,24 @@
-//package com.aoberai.stocktradingbot;
-//
-//import com.crazzyghost.alphavantage.AlphaVantage;
-//import com.crazzyghost.alphavantage.Config;
-//
-//
-//public class StockBotTester {
-//
-//    public static void main(String[] args) {
-//        StockBotTester stockBotTester = new StockBotTester();
-//        stockBotTester.init();
-//    }
-//
-//    public void init() {
-//        Config cfg = Config.builder()
-//                .key(Constants.ALPHAVANTAGE_API_KEY)
-//                .timeOut(Constants.CONNECT_TIMEOUT_MS)
-//                .build();
-//
-//        AlphaVantage.api().init(cfg);
-//
-//        System.out.println("\n\n\nStock Price: " + StockPriceReader.getInstance().getStockPrice(StockPriceReader.TimePeriod.PRICE_MONTH, "AAPL", 6));
-//    }
-//}
+package com.aoberai.stocktradingbot;
+
+import com.crazzyghost.alphavantage.AlphaVantage;
+import com.crazzyghost.alphavantage.Config;
+
+
+public class StockBotTester {
+
+    public static void main(String[] args) {
+        StockBotTester stockBotTester = new StockBotTester();
+        stockBotTester.init();
+    }
+
+    public void init() {
+        Config cfg = Config.builder()
+                .key(Constants.ALPHAVANTAGE_API_KEY)
+                .timeOut(Constants.CONNECT_TIMEOUT_MS)
+                .build();
+
+        AlphaVantage.api().init(cfg);
+
+        System.out.println("\n\n\nStock Price: " + StockPriceReader.getInstance().getStockPrice(StockPriceReader.TimePeriod.PRICE_MONTH, "AAPL", 6));
+    }
+}
